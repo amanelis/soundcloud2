5440ed0aa7418d1de3cfcd08d4eef8fe681f93c2
Alex Manelis <amanelis@gmail.com>
2012-04-14 17:14:18 -0700

    Few changes to method missing args

--- lib/soundcloud2/users.rb
+++ lib/soundcloud2/users.rb
@@ -61,22 +61,32 @@ module Soundcloud2
     
     # GET	/users/{id}/groups	list of joined groups
     def users_groups(*args)
-      options = args.extract_options!.merge(client_id: api_key)
+			options = args.extract_options!.merge(client_id: api_key)
       response = conn.get("/users/#{args[0]}/groups.json") { |req| req.params = options }
       args.nil? ? response.body.send(sym) : response.body
     end
 
-   # def method_missing(sym, *args, &block)
-   #   options = args.extract_options!.merge(client_id: api_key)
-   #   if sym.to_s == "users"
-   #     response = conn.get("/users/#{args[0]}.json") { |req| req.params = options }
-   #   elsif args[1].nil?
-   #     response = conn.get("/users/#{args[0]}.json") { |req| req.params = options }
-   #   else
-   #     response = conn.get("/users/#{args[0]}/#{sym.to_s}/#{args[1]}.json") { |req| req.params = options }
-   #   end
-   #   args.nil? ? response.body.send(sym) : response.body
-   # end
-
+		#def method_missing(sym, *args, &block)
+		#	options 	= args.extrac_options!.merge(client_id: api_key)
+		#	method 		= sym.to_s.gsub(/_(.*)$/, '')
+		#	action 		= sym.to_s.gsub(/^(.*)_/, '')
+		#	param1 		= args[0]
+		#	param2		= args.try(:[], 1).nil? ? ".json" : "/#{args[1]}.json" 
+		#	
+		#	response  = conn.get("/#{method}/#{param1}/#{action}#{param2}"
+		#	args.nil? ? response.body.send(sym) : response.body			
+		#end
+    
+		#def method_missing(sym, *args, &block)
+    #  options = args.extract_options!.merge(client_id: api_key)
+    #  if sym.to_s == "users"
+    #    response = conn.get("/users/#{args[0]}.json") { |req| req.params = options }
+    #  elsif args[1].nil?
+    #    response = conn.get("/users/#{args[0]}.json") { |req| req.params = options }
+    #  else
+    #    response = conn.get("/users/#{args[0]}/#{sym.to_s}/#{args[1]}.json") { |req| req.params = options }
+    #  end
+    #  args.nil? ? response.body.send(sym) : response.body
+    #end
   end
 end
