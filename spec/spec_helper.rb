2ac9a39917d4d8518bcb05fc9a1a111aa2afb1e4
Alex Manelis <amanelis@gmail.com>
2012-07-27 10:35:55 -0700

    Added in simple cov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 require 'simplecov'
 
-#SimpleCov.start do
-  #add_group 'Soundcloud2', 'lib/soundcloud2'
-  #add_group 'Specs', 'spec'
-#end
+SimpleCov.start do
+  add_group 'Soundcloud2', 'lib/soundcloud2'
+  add_group 'Specs', 'spec'
+end
 
 require File.expand_path('../../lib/soundcloud2', __FILE__)
 require 'rubygems'
